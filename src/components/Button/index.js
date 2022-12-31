bc0ad8f07569970f9eefd03a2188dc977ebf64b7
phamchidung <dung6app@gmail.com>
2022-12-31 18:13:14 +0700

    create Header UI part 3

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,13 +3,38 @@ import styles from './Button.module.scss';
 import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
-/**15:48 */
-function Button({ to, href, primary, children, onClick, ...passProps }) {
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    small = false,
+    large = false,
+    text = false,
+    disabled = false,
+    rounded = false,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
     let Comp = 'button';
     const props = {
         onClick,
         ...passProps,
     };
+
+    // Remove event listener when button is disabled
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
 
     if (to) {
         props.to = to;
@@ -20,12 +45,21 @@ function Button({ to, href, primary, children, onClick, ...passProps }) {
     }
 
     const classes = cx('wrapper', {
+        [className]: className,
         primary,
+        outline,
+        small,
+        large,
+        text,
+        disabled,
+        rounded,
     });
 
     return (
         <Comp className={classes} {...props}>
-            <span>{children}</span>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
         </Comp>
     );
 }
