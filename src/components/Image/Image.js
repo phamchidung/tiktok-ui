dd72446be1cd87a5d4c2e87cbed1430e77a5722f
phamchidung <dung6app@gmail.com>
2023-01-06 11:24:50 +0700

    resolve multiple index.js file problem

---
+++ src/components/Image/Image.js
@@ -0,0 +1,35 @@
+import { useState, forwardRef } from 'react';
+import images from '~/assets/images';
+import styles from './Image.module.scss';
+import classNames from 'classnames/bind';
+import PropTypes from 'prop-types';
+
+const cx = classNames.bind(styles);
+
+const Image = forwardRef(({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) => {
+    const [fallback, setFallback] = useState('');
+
+    const handleError = () => {
+        setFallback(customFallback);
+    };
+
+    return (
+        <img
+            className={cx('wrapper', className)}
+            {...props}
+            src={fallback || src}
+            alt={alt}
+            ref={ref}
+            onError={handleError}
+        />
+    );
+});
+
+Image.propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    className: PropTypes.string,
+    fallback: PropTypes.string,
+};
+
+export default Image;
