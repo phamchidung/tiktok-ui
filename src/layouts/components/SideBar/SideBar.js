6dfc59ae8ea634a1d41ab67f3baee3bc0e13ec98
phamchidung <dung6app@gmail.com>
2023-01-09 23:26:42 +0700

    create UI sidebar #1

--- src/layouts/components/SideBar/SideBar.js
+++ src/layouts/components/SideBar/SideBar.js
@@ -1,12 +1,31 @@
 import classNames from 'classnames/bind';
 import styles from './SideBar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import config from '~/config';
+import {
+    HomeIcon,
+    UserGroupIcon,
+    LiveIcon,
+    HomeActiveIcon,
+    UserGroupActiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
 
 const cx = classNames.bind(styles);
 
 function SideBar() {
     return (
         <aside className={cx('wrapper')}>
-            <h2>SideBar</h2>
+            <Menu>
+                <MenuItem title="For you" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="Live" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
         </aside>
     );
 }
